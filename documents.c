fcfeece9b2be6fa3b61e2df72f505a22dab0d43b
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2019-12-02 10:30:24 +1000

    Update documents.c

--- documents.c
+++ documents.c
@@ -9,15 +9,15 @@ int main(){
     fin = fopen("input.txt","r");
     fout = fopen("output.txt", "w");
     int N;
-    fscanf(fin,"%d\n",&N);
-    int K=0;
-    for (int i=1;i<=N;i++){
-        fscanf(fin,"%d ",&A[i]);
-        if (A[i]!=i)
+    fscanf(fin, "%d\n", &N);
+    int K = 0;
+    for (int i = 1;i <= N;i++){
+        fscanf(fin, "%d ",&A[i]);
+        if (A[i] != i)
             K++;
         }
-    fprintf(fout,"%d\n",K*2);
-    for (int i=1;i<=N;i++){
+    fprintf(fout, "%d\n", K*2);
+    for (int i = 1;i <= N;i++){
         exchange(i);
     }
     return 0;
@@ -28,14 +28,14 @@ void exchange(int i){
     if (doc == space){
         return;
     }
-    fprintf(fout,"-%d %d\n",A[i],i);
-    while(doc!=space){
-        int right_doc=A[doc];
-        fprintf(fout,"-%d %d\n",right_doc,doc);
-        fprintf(fout,"%d %d\n",doc,doc);
-        A[doc]=doc;
-        doc=right_doc;
+    fprintf(fout, "-%d %d\n", A[i], i);
+    while(doc != space){
+        int right_doc = A[doc];
+        fprintf(fout, "-%d %d\n", right_doc, doc);
+        fprintf(fout, "%d %d\n", doc, doc);
+        A[doc] = doc;
+        doc = right_doc;
     }
-    fprintf(fout,"%d %d\n",space,doc);
-    A[space]=doc;
+    fprintf(fout, "%d %d\n", space, doc);
+    A[space] = doc;
 }
