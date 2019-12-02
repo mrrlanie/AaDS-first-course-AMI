d08212f6ba2613277d91e03c6259fad4f2810fd8
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2019-12-02 10:32:08 +1000

    Update summing_nums.c

--- summing_nums.c
+++ summing_nums.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    FILE*fin=fopen("input.txt","r");
-    FILE*fout=fopen("output.txt","w");
-    int N ,l,M,r,sum=0;
-    fscanf(fin,"%d",&N);
+    FILE*fin = fopen("input.txt","r");
+    FILE*fout = fopen("output.txt","w");
+    int N , l, M, r, sum=0;
+    fscanf(fin, "%d", &N);
     int A[N+1];
-    A[0]=0;
-    for (int i = 1; i <=N ; ++i) {
-        fscanf(fin,"%d",&A[i]);
+    A[0] = 0;
+    for (int i = 1; i <= N ; ++i) {
+        fscanf(fin, "%d", &A[i]);
     }
-    for (int j = 1; j <=N ; ++j) {
-        A[j]=A[j-1]+A[j];
+    for (int j = 1; j <= N ; ++j) {
+        A[j] = A[j-1] + A[j];
     }
-    fscanf(fin,"%d",&M);
+    fscanf(fin, "%d", &M);
     for (int k = 0; k < M; ++k) {
-        fscanf(fin,"%d%d",&l,&r);
-        fprintf(fout,"%d ",A[r]-A[l-1]);
+        fscanf(fin, "%d%d", &l, &r);
+        fprintf(fout, "%d ", A[r] - A[l-1]);
     }
     fclose(fin);
     fclose(fout);
