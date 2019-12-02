224ad7a24aee36120f5cd950361b8599ccba725d
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2019-12-02 10:31:14 +1000

    Update eggs.c

--- eggs.c
+++ eggs.c
@@ -5,20 +5,20 @@ int main() {
     FILE *fout = fopen("output.txt", "w");
 
     int index_mass;
-    fscanf(fin, "%d", &index_mass);
+    fscanf (fin, "%d", &index_mass);
 
     int mass[index_mass];
-    for(int i =0;i<index_mass;i++){
-        fscanf(fin,"%d", &mass[i]);
+    for(int i = 0;i < index_mass; i++){
+        fscanf(fin, "%d", &mass[i]);
     }
     fclose (fin);
-    for(int i =0;i<index_mass-2;i++){
-        if(mass[i+2]-mass[i]<7){
-            fprintf(fout,"BAD");
+    for(int i = 0;i < index_mass-2; i++){
+        if (mass[i+2] - mass[i] < 7){
+            fprintf (fout, "BAD");
             return 0;
         }
     }
-    fprintf(fout,"GOOD");
+    fprintf (fout, "GOOD");
 fclose (fout);
 return 0;
 }
